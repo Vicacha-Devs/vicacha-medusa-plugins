f4aeea3cc330c067210cd556fe57fc133311f552
Luis Alaguna <alagunasalahaddin@live.com>
2026-09-08 01:36:11 -0500

    feat(shared-admin): add Order Summary Section

--- packages/shared/src/vite-env.d.ts
+++ packages/shared/src/vite-env.d.ts
@@ -1,3 +1,5 @@
 // / <reference types="vite/client" />
 
+declare const __BACKEND_URL__: string | undefined
 declare const __MAX_UPLOAD_FILE_SIZE__: number | undefined
+declare const __STOREFRONT_URL__: string | undefined
